3062f800f1c151ddd8ae2204a679a21ccb544a54
Christian Lewis <christianlewis024@gmail.com>
2020-10-14 12:13:57 -0700

    widget component filled in with twitter embed plugin and styling

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import "./Widgets.css"
+import React from "react";
+import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
-    return (
-        <div className="widgets">
-            <h2>widgets</h2>
-        </div>
-    )
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1316452571951882241"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="_christian_l"
+          options={{ height: 400 }}
+        />
+
+       
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
